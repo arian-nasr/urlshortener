6760eee3a876c4a11194bd3e9d06b3d83a498b36
arian-nasr <81041177+arian-nasr@users.noreply.github.com>
2021-09-13 10:35:30 -0400

    Update Login.vue

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -3,11 +3,11 @@
     <form>
       <div class="form-group">
         <label for="email">Email address</label>
-        <input type="email" class="form-control" id="email" placeholder="Enter email">
+        <input type="email" class="form-control" id="email" placeholder="Enter email" v-model="email">
       </div>
       <div class="form-group">
         <label for="password">Password</label>
-        <input type="password" class="form-control" id="password" placeholder="Password">
+        <input type="password" class="form-control" id="password" placeholder="Password" v-model="password">
       </div>
       <button type="button" class="btn btn-primary" @click="submitform">Submit</button>
     </form>
