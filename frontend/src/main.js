d6c7c21d85e270ca3fb4f02149eebfeee068860a
arian-nasr <81041177+arian-nasr@users.noreply.github.com>
2021-09-12 22:12:50 -0400

    bug fixes

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.css'
 
 Vue.config.productionTip = false
 
