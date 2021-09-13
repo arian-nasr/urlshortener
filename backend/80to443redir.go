0f46b235953f85b2b039a0243d99092175a078dc
Arian Nasr <anasr@my.yorku.ca>
2021-09-13 16:47:33 -0400

    Update 80to443redir.go

--- backend/80to443redir.go
+++ backend/80to443redir.go
@@ -7,5 +7,5 @@ func main() {
 	r.GET("/*path", func(c *gin.Context) {
 		c.Redirect(302, "https://onebounce.me/" + c.Param("variable"))
 	})
-	r.Run("10.128.0.3:80") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run("10.128.0.3:80")
 }
