dc0677fb93d1ecd456242b2ac8e84cf20887182b
arian-nasr <81041177+arian-nasr@users.noreply.github.com>
2021-09-13 21:13:57 -0400

    Update Login.vue

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="modal modal-signin position-static d-block bg-secondary py-5" tabindex="-1" role="dialog" id="modalSignin">
+  <div class="modal modal-signin position-static d-block py-5" tabindex="-1" role="dialog" id="modalSignin">
     <div class="modal-dialog" role="document">
       <div class="modal-content rounded-5 shadow">
         <div class="modal-header p-5 pb-4 border-bottom-0">
