7ff4b119152e4af443e98746c4cf35e8d62d96f2
arian-nasr <81041177+arian-nasr@users.noreply.github.com>
2021-09-14 09:38:32 -0400

    facelift

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -4,7 +4,7 @@
       <div class="modal-content rounded-5 shadow">
         <div class="modal-header p-5 pb-4 border-bottom-0">
           <!-- <h5 class="modal-title">Modal title</h5> -->
-          <h2 class="fw-bold mb-0 text-center">URL Shortener</h2>
+          <h2 class="fw-bold mb-0">URL Shortener</h2>
         </div>
         <div class="modal-body p-5 pt-0">
           <form class="">
