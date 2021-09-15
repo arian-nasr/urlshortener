3d5c930265d0b200f4520eb7d3427cdc0225c6d9
arian-nasr <81041177+arian-nasr@users.noreply.github.com>
2021-09-15 19:29:51 -0400

    Update Index.vue

--- frontend/src/components/Index.vue
+++ frontend/src/components/Index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="neededdiv">
     <header>
-      <div class="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom">
+      <div class="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom neededdiv">
         <a href="/" class="d-flex align-items-center text-dark text-decoration-none">
           <span class="fs-4">OneBounce URL Shortener</span>
         </a>
