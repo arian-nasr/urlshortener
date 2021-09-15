6affa41da599ddcb90cbdca39841ca2e5951d8d2
arian-nasr <81041177+arian-nasr@users.noreply.github.com>
2021-09-15 19:36:31 -0400

    Update Index.vue

--- frontend/src/components/Index.vue
+++ frontend/src/components/Index.vue
@@ -3,11 +3,11 @@
     <header>
       <div class="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom">
         <a href="/" class="d-flex align-items-center text-dark text-decoration-none">
-          <span class="fs-4 logospacing">OneBounce URL Shortener</span>
+          <span class="fs-4 logospacing">OneBounce</span>
         </a>
 
         <nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto">
-          <a class="me-3 py-2 text-dark text-decoration-none" href="/login">Login</a>
+          <a class="me-3 py-2 text-dark text-decoration-none loginspacing" href="/login">Login</a>
         </nav>
       </div>
 
@@ -137,6 +137,11 @@ export default {
 }
 
 .logospacing {
+  padding-left: 1rem;
+  padding-top: 1rem;
+}
+
+.loginspacing {
   padding-left: 1rem;
   padding-top: 1rem;
 }
