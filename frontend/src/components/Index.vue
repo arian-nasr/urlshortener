23ed05c52b7e1b5aaa53b6430e5f5eb645c5dd2d
arian-nasr <81041177+arian-nasr@users.noreply.github.com>
2021-09-15 19:37:54 -0400

    Update Index.vue

--- frontend/src/components/Index.vue
+++ frontend/src/components/Index.vue
@@ -6,8 +6,8 @@
           <span class="fs-4 logospacing">OneBounce</span>
         </a>
 
-        <nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto">
-          <a class="me-3 py-2 text-dark text-decoration-none loginspacing" href="/login">Login</a>
+        <nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto loginspacing">
+          <a class="me-3 py-2 text-dark text-decoration-none" href="/login">Login</a>
         </nav>
       </div>
 
